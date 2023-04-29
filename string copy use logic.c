f0648eb7b1745284dd6326e46b6879621aa0e89d
ArmanSardar <mdarmansardar57@gmail.com>
2023-04-29 21:19:35 +0600

    pitron practice

--- string copy use logic.c
+++ string copy use logic.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+    char ar [84];
+    char br [89];
+    scanf("%s %s",&ar,&br);
+    for (int i =0; i<=strlen(br); i++)
+    {
 
+
+        ar[i]=br[i];
+    }
+    printf("%s %s",ar,br);
+}
