458f449c17c3a818fba51c8dcb540fd9fe53fa67
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-01 14:53:58 +0600

    pitron practice

---
+++ string lexographical comparisin use logic.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    char a[100],b[100];
+    scanf("%s %s",&a,&b);
+    int i =0;
+    while (1)
+    {
+        if (a[i]=='\0' && b[i]=='\0')
+        {
+            printf("same\n");
+            break;
+        }
+        else if (a[i]=='\0')
+        {
+            printf ("a choto");
+            break ;
+        }
+        else if (b[i]=='\0')
+        {
+            printf ("b choto");
+            break;
+        }
+        if (a[i]==b[i])
+        {
+            i++;
+        }
+        else if (a[i]<b[i])
+        {
+            printf(" A CHOTO");
+            break;
+        }
+        else
+        {
+            printf("B CHOTO");
+            break;
+        }
+    }
+}
