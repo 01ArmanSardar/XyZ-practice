f155a3ae5e545b4edb3aa4dcb7c40cabef3a584e
ArmanSardar <mdarmansardar57@gmail.com>
2023-08-25 00:27:07 +0600

    pitron practice

---
+++ code in Vs Code/insert_at_the_tail.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void InsertAtTail(Node *&head, int v)
+{
+    Node *newnode = new Node(v);
+    if (head == NULL)
+    {
+        head = newnode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next = NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newnode;
+}
+void PrintLinkedList(Node *head)
+{
+    Node *tmp=head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val<< " ";
+        tmp =tmp->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    cout << "option 1;insetr at the tail" << endl;
+    int op;
+    cin >> op;
+    if (op == 1)
+    {
+        int v;
+        cin >> v;
+        InsertAtTail(head, v);
+    }
+    PrintLinkedList(head);
+}
